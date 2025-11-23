59686fdab26564179a95714ead0606737a9c72a9
Alfie Littlestone <alfielittlestone@gmail.com>
2025-11-23 21:27:41 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //@IntercityCheese
-      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
